9c44b84904ea2d15ff6b6385ff3c48e48f847ff4
trevorranderson <trevorranderson2012@gmail.com>
2020-03-18 11:09:53 -0700

    Deployment

--- jabber_user.py
+++ jabber_user.py
@@ -39,97 +39,344 @@ user_import_data = pd.read_excel("User_Import.xlsx")
 
 ## ADD DN
 
-# index = 0
-# while index < len(user_import_data.index):
-#     dn = user_import_data.at[index, 'DIRECTORYNUMBER']
-#     desc = user_import_data.at[index, 'DESCRIPTION']
-#     part = user_import_data.at[index, 'PARTITION']
-#     line = {
-#         'pattern': dn,
-#         'description': desc,
-#         'usage': 'Device',
-#         'routePartitionName': part,
-#         'alertingName': desc,
-#         'asciiAlertingName': desc
-#     }
-#     try:
-#         service.addLine(line)
-#     except:
-#         pass        
-#     index += 1
-
-## ADD CSF DEVICE PER USER
-
-# index = 0
-# while index < len(user_import_data.index):
-#     name = user_import_data.at[index, 'CSFNAME']
-#     css = user_import_data.loc[index, 'DEVICECSS']
-#     dp = user_import_data.loc[index, 'DEVICEPOOL']
-#     dn = user_import_data.at[index, 'DIRECTORYNUMBER']
-#     part = user_import_data.at[index, 'PARTITION']
-#     phone = {
-#         'name': 'CSF' + name,
-#         'description': "For Jabber Use",
-#         'product': 'Cisco Unified Client Services Framework',
-#         'model': 'Cisco Unified Client Services Framework',
-#         'class': 'Phone',
-#         'protocol': 'SIP',
-#         'protocolSide': 'User',
-#         'callingSearchSpaceName': css,
-#         'devicePoolName': dp,
-#         'commonPhoneConfigName': 'Standard Common Phone Profile',
-#         'locationName': 'Hub_None',
-#         'useTrustedRelayPoint': 'Default',
-#         'builtInBridgeStatus': 'Default',
-#         'packetCaptureMode': 'None',
-#         'certificateOperation': 'No Pending Operation',
-#         'deviceMobilityMode': 'Default',
-#         'lines': {
-#             'line': [
-#                 {
-#                     'index': 1,
-#                     'dirn': {
-#                         'pattern': dn,
-#                         'routePartitionName': part
-#                     }
-#                 }
-#             ]
-#         }
-#     }
-#     try:
-#         service.addPhone(phone)
-#     except:
-#         pass        
-#     index += 1
-    
-## CREATE USER
-
 index = 0
 while index < len(user_import_data.index):
-    device = user_import_data.at[index, 'CSFNAME']
-    associated_device = {
-        'device': []
+    dn = user_import_data.at[index, 'DIRECTORYNUMBER']
+    desc = user_import_data.at[index, 'DESCRIPTION']
+    part = user_import_data.at[index, 'PARTITION']
+    line = {
+        'pattern': dn,
+        'description': desc,
+        'usage': 'Device',
+        'routePartitionName': part,
+        'alertingName': desc,
+        'asciiAlertingName': desc
     }
-    associated_device['device'].append('CSF' + user_import_data.at[index, 'CSFNAME'])
-    userid = user_import_data.at[index, 'USER']
-    first = user_import_data.at[index, 'FIRST']
-    last = user_import_data.at[index, 'LAST']
-    pw = user_import_data.at[index, 'PASSWORD']
-    end_user = {
-        'userid': userid,
-        'firstName': first,
-        'lastName': last,
-        'password': pw,
-        'presenceGroupName': 'Standard Presence Group',
-        'associatedDevices': associated_device
-    }
-    service.addUser(end_user)   
+    try:
+        service.addLine(line)
+    except:
+        pass        
     index += 1
+
+## ADD DEVICE PER DEVICE TYPE
+index = 0
+
+while index < len(user_import_data.index):
+
+    ## ADD DEVICE FOR DESKTOP 
+ 
+    if user_import_data.at[index, 'DEVICETYPE'] == 'DESKTOP':
+        name = user_import_data.at[index, 'DEVICENAME']
+        css = user_import_data.loc[index, 'DEVICECSS']
+        dp = user_import_data.loc[index, 'DEVICEPOOL']
+        dn = user_import_data.at[index, 'DIRECTORYNUMBER']
+        part = user_import_data.at[index, 'PARTITION']
+        desc = user_import_data.at[index, 'DESCRIPTION']
+        phone = {
+            'name': 'CSF' + name,
+            'description': "For Jabber Use",
+            'product': 'Cisco Unified Client Services Framework',
+            'model': 'Cisco Unified Client Services Framework',
+            'class': 'Phone',
+            'protocol': 'SIP',
+            'protocolSide': 'User',
+            'callingSearchSpaceName': css,
+            'devicePoolName': dp,
+            'commonPhoneConfigName': 'Standard Common Phone Profile',
+            'locationName': 'Hub_None',
+            'useTrustedRelayPoint': 'Default',
+            'builtInBridgeStatus': 'Default',
+            'packetCaptureMode': 'None',
+            'certificateOperation': 'No Pending Operation',
+            'deviceMobilityMode': 'Default',
+            'lines': {
+                'line': [
+                    {
+                        'index': 1,
+                        'dirn': {
+                            'pattern': dn,
+                            'routePartitionName': part
+                        }
+                    }
+                ]
+            }
+        }
+        try:
+            service.addPhone(phone)
+        except:
+            pass
+
+        ## ADD USER FOR DESKTOP      
+
+        device = user_import_data.at[index, 'DEVICENAME']
+        associated_device = {
+            'device': []
+        }
+        associated_device['device'].append('CSF' + user_import_data.at[index, 'DEVICENAME'])
+        userid = user_import_data.at[index, 'USER']
+        first = user_import_data.at[index, 'FIRST']
+        last = user_import_data.at[index, 'LAST']
+        pw = user_import_data.at[index, 'PASSWORD']
+        try:
+            dn = int(user_import_data.at[index, 'DIRECTORYNUMBER'])
+        except:
+            pass
+        end_user = {
+            'userid': userid,
+            'firstName': first,
+            'lastName': last,
+            'password': pw,
+            'telephoneNumber': dn,
+            'presenceGroupName': 'Standard Presence Group',
+            'associatedDevices': associated_device
+                }
+        try:
+            service.addUser(end_user)
+        except:
+            pass
+
+    ## ADD DEVICE FOR TABLET
+
+    if user_import_data.at[index, 'DEVICETYPE'] == 'TABLET':
+        name = user_import_data.at[index, 'DEVICENAME']
+        css = user_import_data.loc[index, 'DEVICECSS']
+        dp = user_import_data.loc[index, 'DEVICEPOOL']
+        dn = user_import_data.at[index, 'DIRECTORYNUMBER']
+        part = user_import_data.at[index, 'PARTITION']
+        desc = user_import_data.at[index, 'DESCRIPTION']
+        phone = {
+            'name': 'TAB' + name,
+            'description': "For Jabber Use - Tablet",
+            'product': 'Cisco Jabber for Tablet',
+            'model': 'Cisco Jabber for Tablet',
+            'class': 'Phone',
+            'protocol': 'SIP',
+            'protocolSide': 'User',
+            'callingSearchSpaceName': css,
+            'devicePoolName': dp,
+            'commonPhoneConfigName': 'Standard Common Phone Profile',
+            'locationName': 'Hub_None',
+            'useTrustedRelayPoint': 'Default',
+            'builtInBridgeStatus': 'Default',
+            'packetCaptureMode': 'None',
+            'certificateOperation': 'No Pending Operation',
+            'deviceMobilityMode': 'Default',
+            'lines': {
+                'line': [
+                    {
+                        'index': 1,
+                        'dirn': {
+                            'pattern': dn,
+                            'routePartitionName': part
+                        }
+                    }
+                ]
+            }
+        }
+        try:
+            service.addPhone(phone)
+        except:
+            pass
+
+        ## ADD USER FOR TABLET      
+
+        device = user_import_data.at[index, 'DEVICENAME']
+        associated_device = {
+            'device': []
+        }
+        associated_device['device'].append('TAB' + user_import_data.at[index, 'DEVICENAME'])
+        userid = user_import_data.at[index, 'USER']
+        first = user_import_data.at[index, 'FIRST']
+        last = user_import_data.at[index, 'LAST']
+        pw = user_import_data.at[index, 'PASSWORD']
+        dn = user_import_data.at[index, 'DIRECTORYNUMBER']
+        end_user = {
+            'userid': userid,
+            'firstName': first,
+            'lastName': last,
+            'password': pw,
+            'telephoneNumber': dn,
+            'presenceGroupName': 'Standard Presence Group',
+            'associatedDevices': associated_device
+                }
+        try:
+            service.addUser(end_user)
+        except:
+            pass
+
+
+    ## ADD DEVICE FOR IPHONE
+
+    if user_import_data.at[index, 'DEVICETYPE'] == 'IPHONE':
+        name = user_import_data.at[index, 'DEVICENAME']
+        css = user_import_data.loc[index, 'DEVICECSS']
+        dp = user_import_data.loc[index, 'DEVICEPOOL']
+        dn = user_import_data.at[index, 'DIRECTORYNUMBER']
+        part = user_import_data.at[index, 'PARTITION']
+        desc = user_import_data.at[index, 'DESCRIPTION']
+        phone = {
+            'name': 'TCT' + name,
+            'description': "For Jabber Use - iPhone",
+            'product': 'Cisco Dual Mode for iPhone',
+            'model': 'Cisco Dual Mode for iPhone',
+            'class': 'Phone',
+            'protocol': 'SIP',
+            'protocolSide': 'User',
+            'callingSearchSpaceName': css,
+            'devicePoolName': dp,
+            'commonPhoneConfigName': 'Standard Common Phone Profile',
+            'locationName': 'Hub_None',
+            'useTrustedRelayPoint': 'Default',
+            'builtInBridgeStatus': 'Default',
+            'packetCaptureMode': 'None',
+            'certificateOperation': 'No Pending Operation',
+            'deviceMobilityMode': 'Default',
+            'lines': {
+                'line': [
+                    {
+                        'index': 1,
+                        'dirn': {
+                            'pattern': dn,
+                            'routePartitionName': part
+                        }
+                    }
+                ]
+            }
+        }
+
+        try:
+            service.addPhone(phone)
+        except:
+            pass
+
+        ## ADD USER FOR IPHONE      
+
+        device = user_import_data.at[index, 'DEVICENAME']
+        associated_device = {
+            'device': []
+        }
+        associated_device['device'].append('TCT' + user_import_data.at[index, 'DEVICENAME'])
+        userid = user_import_data.at[index, 'USER']
+        first = user_import_data.at[index, 'FIRST']
+        last = user_import_data.at[index, 'LAST']
+        pw = user_import_data.at[index, 'PASSWORD']
+        try:
+            dn = int(user_import_data.at[index, 'DIRECTORYNUMBER'])
+        except:
+            pass
+        end_user = {
+            'userid': userid,
+            'firstName': first,
+            'lastName': last,
+            'password': pw,
+            'telephoneNumber': dn,
+            'presenceGroupName': 'Standard Presence Group',
+            'associatedDevices': associated_device
+                }
+        try:
+            service.addUser(end_user)
+        except:
+            pass
+
+    ## ADD DEVICE FOR ANDROID
+
+    if user_import_data.at[index, 'DEVICETYPE'] == 'ANDROID':
+        name = user_import_data.at[index, 'DEVICENAME']
+        css = user_import_data.loc[index, 'DEVICECSS']
+        dp = user_import_data.loc[index, 'DEVICEPOOL']
+        dn = user_import_data.at[index, 'DIRECTORYNUMBER']
+        part = user_import_data.at[index, 'PARTITION']
+        desc = user_import_data.at[index, 'DESCRIPTION']
+        phone = {
+            'name': 'BOT' + name,
+            'description': "For Jabber Use - Android",
+            'product': 'Cisco Dual Mode for Android',
+            'model': 'Cisco Dual Mode for Android',
+            'class': 'Phone',
+            'protocol': 'SIP',
+            'protocolSide': 'User',
+            'callingSearchSpaceName': css,
+            'devicePoolName': dp,
+            'commonPhoneConfigName': 'Standard Common Phone Profile',
+            'locationName': 'Hub_None',
+            'useTrustedRelayPoint': 'Default',
+            'builtInBridgeStatus': 'Default',
+            'packetCaptureMode': 'None',
+            'certificateOperation': 'No Pending Operation',
+            'deviceMobilityMode': 'Default',
+            'lines': {
+                'line': [
+                    {
+                        'index': 1,
+                        'dirn': {
+                            'pattern': dn,
+                            'routePartitionName': part
+                        }
+                    }
+                ]
+            }
+        }
+
+        try:
+            service.addPhone(phone)
+        except:
+            pass
+
+        ## ADD USER FOR IPHONE      
+
+        device = user_import_data.at[index, 'DEVICENAME']
+        associated_device = {
+            'device': []
+        }
+        associated_device['device'].append('BOT' + user_import_data.at[index, 'DEVICENAME'])
+        userid = user_import_data.at[index, 'USER']
+        first = user_import_data.at[index, 'FIRST']
+        last = user_import_data.at[index, 'LAST']
+        pw = user_import_data.at[index, 'PASSWORD']
+        try:
+            dn = int(user_import_data.at[index, 'DIRECTORYNUMBER'])
+        except:
+            pass
+        end_user = {
+            'userid': userid,
+            'firstName': first,
+            'lastName': last,
+            'password': pw,
+            'telephoneNumber': dn,
+            'presenceGroupName': 'Standard Presence Group',
+            'associatedDevices': associated_device
+                }
+        try:
+            service.addUser(end_user)
+        except:
+            pass
+    else:
+        print(user_import_data.at[index, 'USER'] + " was not created. Is likely a duplicate.")
+    index += 1
+
+    ## ADD USER FOR ANDROID
 
 ## UPDATE HOME CLUSTER AND ENABLE FOR IMP
 
-# for user in user_import_data['USER']:
-#     try:
-#         service.updateUser(userid=user, homeCluster=True, imAndPresenceEnable=True)
-#     except:
-#         pass
+for user in user_import_data['USER']:
+    try:
+        service.updateUser(userid=user, homeCluster=True, imAndPresenceEnable=True)
+    except:
+        pass
+
+## ADD USER GROUP
+# members = []
+# index = 0
+# while index < len(user_import_data.index):
+#     members.append(user_import_data.at[index, 'USER'])
+#     index += 1
+# user_roles = ['Standard CCM End Users', 'Standard CTI Enabled', 'Standard CTI Allow Control of All Devices']
+# jabber_user_group = {
+#     'name': 'Jabber_Users',
+#     'userRoles': user_roles,
+#     'members': members
+
+# }
+# service.addUserGroup(jabber_user_group)
+
+print("Complete!")
